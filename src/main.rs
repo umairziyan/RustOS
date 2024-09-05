309fd2efc13dd1b33cb68076cc8742fc603264cc
= <umairziyan@protonmail.com>
2024-09-05 16:55:51 +0400

    Poor commit

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@
 #![no_main] // disable all Rust-level entry points
 
 use core::panic::PanicInfo;
+mod vga_buffer;
 
+static HELLO: &[u8] = b"Hello World!";
 // This disables name mangling to ensure that the rust compiler really outputs a function with the
 // name _start.
 // This function is diverging because the entry point is never called by any function but invoked
@@ -10,6 +12,16 @@ use core::panic::PanicInfo;
 // invoke the exit system call of the operating sytem.
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    use core::fmt::Write;
+    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+    write!(
+        vga_buffer::WRITER.lock(),
+        ", some numbers: {} {}",
+        42,
+        1.337
+    )
+    .unwrap();
+
     loop {}
 }
 
