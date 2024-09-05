5192f8118828083b9e79622703778b0b232b50eb
= <umairziyan@protonmail.com>
2024-09-05 17:01:55 +0400

    Added println! macro

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@
 use core::panic::PanicInfo;
 mod vga_buffer;
 
-static HELLO: &[u8] = b"Hello World!";
 // This disables name mangling to ensure that the rust compiler really outputs a function with the
 // name _start.
 // This function is diverging because the entry point is never called by any function but invoked
@@ -12,16 +11,7 @@ static HELLO: &[u8] = b"Hello World!";
 // invoke the exit system call of the operating sytem.
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    write!(
-        vga_buffer::WRITER.lock(),
-        ", some numbers: {} {}",
-        42,
-        1.337
-    )
-    .unwrap();
-
+    println!("Hello World{}", "!");
     loop {}
 }
 
