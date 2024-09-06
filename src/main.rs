af87e1a6c8c78d6020038d933c040a4ad5e58c76
= <umairziyan@protonmail.com>
2024-09-06 11:48:18 +0400

    Added test runner
    There are no tests as they are part of the standard library which has
    been disabled.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
+// Test framework
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
 use core::panic::PanicInfo;
 mod vga_buffer;
 
@@ -12,7 +17,11 @@ mod vga_buffer;
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
-    panic!("Some panic message");
+
+    // Conditional compliation to add the call to test_main only in test contexts.
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
@@ -22,4 +31,19 @@ pub extern "C" fn _start() -> ! {
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion...");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
