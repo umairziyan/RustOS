b1aad27c5911c62a8383608331e0cd53144b49dc
= <umairziyan@protonmail.com>
2024-09-06 16:37:23 +0400

    Moved everything from main into a library and setup integration tests.

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,11 @@
 
 // Test framework
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(rust_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-mod serial;
-mod vga_buffer;
+use rust_os::println;
 
 // This disables name mangling to ensure that the rust compiler really outputs a function with the
 // name _start.
@@ -26,68 +25,6 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
-// Function to call on panic.
-// PanicInfo contains the file and line where the panic happened.
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-// panic handler in test mode
-#[cfg(test)]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
-}
-
-#[cfg(test)]
-pub fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-    // exit if successful
-    exit_qemu(QemuExitCode::Success);
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        // iobase of trhe isa-debug-exit device.
-        let mut port = Port::new(0xf4);
-
-        // Write exit code to the port.
-        port.write(exit_code as u32);
-    }
-}
-
-pub trait Testable {
-    fn run(&self);
-}
-
-impl<T> Testable for T
-where
-    T: Fn(),
-{
-    fn run(&self) {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
-}
-
 // tests
 #[test_case]
 fn test_println_simple() {
@@ -99,4 +36,18 @@ fn test_println_many() {
     for _ in 0..200 {
         println!("test_println_many output");
     }
+}
+
+/// This function is called on panic.
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    rust_os::test_panic_handler(info)
 }
