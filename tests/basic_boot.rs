86454f34d73c146003b2fbe698032a4fc7099f9d
= <umairziyan@protonmail.com>
2024-09-06 16:47:29 +0400

    Set up basic integration tests

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -1,10 +1,11 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(rust_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use rust_os::println;
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
@@ -13,11 +14,12 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
-fn test_runner(tests: &[&dyn Fn()]) {
-    unimplemented!();
-}
-
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    loop {}
+    rust_os::test_panic_handler(info)
+}
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
