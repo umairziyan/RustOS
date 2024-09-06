4d947442595b6350d0c88aca0d433897a80fabef
= <umairziyan@protonmail.com>
2024-09-06 16:12:41 +0400

    Prints automatically

--- src/main.rs
+++ src/main.rs
@@ -45,10 +45,10 @@ fn panic(info: &PanicInfo) -> ! {
 }
 
 #[cfg(test)]
-pub fn test_runner(tests: &[&dyn Fn()]) {
+pub fn test_runner(tests: &[&dyn Testable]) {
     serial_println!("Running {} tests", tests.len());
     for test in tests {
-        test();
+        test.run();
     }
     // exit if successful
     exit_qemu(QemuExitCode::Success);
@@ -56,9 +56,7 @@ pub fn test_runner(tests: &[&dyn Fn()]) {
 
 #[test_case]
 fn trivial_assertion() {
-    serial_print!("trivial assertion...");
-    assert_eq!(1, 0);
-    serial_println!("[ok]");
+    assert_eq!(1, 1);
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -77,5 +75,20 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
 
         // Write exit code to the port.
         port.write(exit_code as u32);
+    }
+}
+
+pub trait Testable {
+    fn run(&self);
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
     }
 }
